ccd143a43241ef3b81056536f46b6917ef949838
José Henrique <55933448+DevJHenrique@users.noreply.github.com>
2023-06-11 18:49:19 +0000

    add home page

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+function Home() {
+    const homePage = (
+
+        <div>
+            <p align="center"></p>
+            <title>EngNews</title>
+            <h1 align="center">
+                EM CONSTRUÇÃO!
+            </h1>
+
+            <p align="center">
+                Em breve um novo
+                portal de notícias voltadas para a área da Engenharia.
+            </p>
+        </div>
+    )
+    return homePage;
+}
+
+export default Home;
